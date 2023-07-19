3cd017475161cbbec67b16ee57ddc4c74f5ac5b4
Praket Chavan <praket.chavan@gmail.com>
2023-07-19 18:06:04 +0100

    Add Movie component to display movies
    Add a movie component to display all the movie information.
    It is styled using the bootstrap card component.
    
    Updated the App.tsx file to remove the generated code and display
    the new movie component.
    
    Remove redudant file: index.css

---
+++ src/components/movie/Movie.tsx
@@ -0,0 +1,23 @@
+interface MovieProp {
+  name: string;
+  releaseDate?: Date;
+  imdbId?: string;
+  trailerLink?: string;
+}
+
+function Movie({ name, releaseDate, trailerLink }: MovieProp) {
+  return (
+    <div className="card col-4">
+      <div className="car-body">
+        <h6 className="card-title">{name}</h6>
+        <p className="card-text">This is the movie text</p>
+        <p className="card-text">{releaseDate?.toDateString()}</p>
+        <a className="card-link" href={trailerLink}>
+          Trailer Link
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
