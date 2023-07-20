450b526569d5724a6c19021b7427130ee431ac3d
Praket Chavan <praket.chavan@gmail.com>
2023-07-20 14:28:10 +0100

    Fix error in code

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,21 @@ import axios, { AxiosResponse } from "axios";
 import { MovieType } from "../model/movie.interface";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:8080/api/v1/",
     timeout: 50000
 });
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const request =  {
+    get: (url: string) => instance.get(url).then(responseBody),
+    post: (url: string, body: {}) => instance.post(url, body).then(responseBody)
+};
+
+export const Movie = {
+    getPosts: (): Promise<MovieType[]> => request.get('movie/all')
+};
+
+export const Review = {
+
+}
