938898a8f49b8da9a180a49dafab57880e8924b0
Praket Chavan <praket.chavan@gmail.com>
2023-07-24 13:07:34 +0100

    Add navigation functionality to the project
    Add react router dom elements to add navigation functionilty.
    Set up default url for the app
    Add on click listerner on the movie card to navigate the user to the
    review form component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MovieCardGroup from "./components/movie/MovieCardGroup";
+import ReviewForm from "./components/review/ReviewForm";
 
 function App() {
   return (
     <>
-      <MovieCardGroup />
+      <Routes>
+        <Route path="/" element={<Navigate to="/movies" />} />
+        <Route path="/movies" element={<MovieCardGroup />} />
+        <Route path="/movies/:movieId" element={<ReviewForm />} />
+      </Routes>
+      {/* <MovieCardGroup /> */}
     </>
   );
 }
--- src/components/movie/MovieCard.tsx
+++ src/components/movie/MovieCard.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { MovieType } from "../../model/movie.interface";
-
 interface MovieProp {
   movie: MovieType;
 }
 
 function MovieCard({ movie }: MovieProp) {
+  const navigate = useNavigate();
+
+  function navigateToMovie(movieId: string) {
+    navigate(movieId);
+  }
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => navigateToMovie(movie.imdbId)}>
       <div className="car-body">
         <h6 className="card-title">{movie.title}</h6>
         <p className="card-text">This is the movie text</p>
